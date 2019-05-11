2864d057e7d84e854f3a92ad7125affd43626e18
kukulam1 <32060601+kukulam1@users.noreply.github.com>
2019-05-11 12:25:26 +0200

    Add files via upload

--- upperlower.sh
+++ upperlower.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-if [ "$1" != "-r" ] ; then
-     for i in $(find ./ -type f); do    
-          if [ "$(echo $i | sed 's/\(.*\)\/\(.*\)/\1\/\U\2/')" = "$i" ]; then 
-                 mv $i $(echo $i | sed 's/\(.*\)\/\(.*\)/\1\/\L\2/') 
-          fi 
-     done
-else
-   for i in $(find ./ -type f); do
-         if [ "$( echo $i | sed 's/\(.*\)\/\(.*\)/\1\/\L\2/')" = "$i" ]; then
-                 mv $i $(echo $i | sed 's/\(.*\)\/\(.*\)/\1\/\U\2/') 
-         fi 
-     done
+# opraveny mezery v nazvech souboru a parametry skriptu
+if [ $# = 2 -a "$1" = "-r" ] ; then
+    find "$2" -type f -exec sh -c '
+          if [ "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\L\2/'\'')" = "$0" ]; then
+                 mv "$0" "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\U\2/'\'')"
+          fi ' {} ';'
+elif
+  [ "$1" = "-r" ]; then
+    find ./ -type f -exec sh -c '
+          if [ "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\L\2/'\'')" = "$0" ]; then
+                 mv "$0" "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\U\2/'\'')"
+          fi ' {} ';'
+elif 
+  [ "$1" != "" ]; then
+    find "$1" -type f -exec sh -c '
+          if [ "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\U\2/'\'')" = "$0" ]; then
+                 mv "$0" "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\L\2/'\'')"
+          fi ' {} ';'     
+else 
+    find ./ -type f -exec sh -c '
+          if [ "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\U\2/'\'')" = "$0" ]; then
+                 mv "$0" "$(echo "$0" | sed '\''s/\(.*\)\/\(.*\)/\1\/\L\2/'\'')"
+          fi ' {} ';'
 fi
